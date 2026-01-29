3a0db2a86d624382e15421fbc08fd3c8136ccccd
Vishal Attri <vishalattri196@gmail.com>
2026-01-29 20:17:27 +0530

    Initial commit done for Hacker-Rank Questions

---
+++ HACKERRANK-QUESTIONS/first_four_character.sh
@@ -0,0 +1,4 @@
+# Display the first four characters from each line of text.
+
+cut -c1-4
+
---
+++ HACKERRANK-QUESTIONS/line_character.sh
@@ -0,0 +1,6 @@
+# Display 2nd the 7th and  character from each line of text.
+
+cut -c2,7
+
+# when we want to extarct specific characters from each line of input.
+# then we use comma to separate the character positions.
